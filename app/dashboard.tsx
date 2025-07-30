1f782c6d582a01999160d1efc447c6b8537d2dbd
paragrghoshal <ghoshsar000@students.centergrove.k12.in.us>
2025-07-29 23:41:11 -0400

    added more bento buttons for more stuff (i hate myself)

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -5,7 +5,7 @@ import { Fonts } from "@/hooks/useFont";
 import { AntDesign, FontAwesome6, MaterialIcons } from '@expo/vector-icons';
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Dashboard() {
@@ -52,24 +52,54 @@ export default function Dashboard() {
               </TouchableOpacity>
 
               <TouchableOpacity onPress={() => {router.push("/planner")}}>
-                <BentoBox size="small" className="border-cyan-400 w-40 h-40 rounded-3xl" style={{ backgroundColor: "rgba(6, 182, 212, 0.3)", borderWidth: 1 }}>
+                <BentoBox size="small" className="border-cyan-400 w-40 h-40 rounded-3xl mb-5" style={{ backgroundColor: "rgba(6, 182, 212, 0.3)", borderWidth: 1 }}>
                   <MaterialIcons name="draw" size={30} color="white" />
                   <Text className="text-white mt-2 text-3xl" style={{ fontFamily:Fonts.Inter }}>Planner</Text>
                 </BentoBox>
               </TouchableOpacity>
 
               <TouchableOpacity onPress={() => {router.push("/rank")}}>
-                <BentoBox size="small" className="border-purple-400 w-40 h-40 rounded-3xl" style={{ backgroundColor: "rgba(168, 85, 247, 0.3)", borderWidth: 1 }}>
+                <BentoBox size="small" className="border-purple-400 w-40 h-40 rounded-3xl mb-5" style={{ backgroundColor: "rgba(168, 85, 247, 0.3)", borderWidth: 1 }}>
                   <FontAwesome6 name="ranking-star" size={30} color="white" />
                   <Text className="text-white mt-2 text-3xl" style={{ fontFamily:Fonts.Inter }}>Ranks</Text>
                 </BentoBox>
               </TouchableOpacity>
-            </BentoGrid>
-            <View className="mb-28" />
 
-            <Text id="data" className="">
+              <TouchableOpacity onPress={() => {}}>
+                <BentoBox size="small" className="border-lime-400 w-40 h-40 rounded-3xl mb-5" style={{ backgroundColor: "rgba(132, 204, 22, 0.3)", borderWidth: 1 }}>
+                  <FontAwesome6 name="wand-magic-sparkles" size={30} color="white" />
+                  <Text className="text-white mt-2 text-3xl" style={{ fontFamily:Fonts.Inter }}>Predictor</Text>
+                </BentoBox>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => {}}>
+                <BentoBox size="small" className="border-orange-400 w-40 h-40 rounded-3xl mb-5" style={{ backgroundColor: "rgba(249, 115, 22, 0.3)", borderWidth: 1 }}>
+                  <FontAwesome6 name="magnifying-glass" size={30} color="white" />
+                  <Text className="text-white mt-2 text-3xl" style={{ fontFamily:Fonts.Inter }}>Lookup</Text>
+                </BentoBox>
+              </TouchableOpacity>
               
-            </Text>
+              <TouchableOpacity onPress={() => {}}>
+                <BentoBox size="small" className="border-green-400 w-40 h-40 rounded-3xl mb-5" style={{ backgroundColor: "rgba(34, 197, 94, 0.3)", borderWidth: 1 }}>
+                  <AntDesign name="calendar" size={30} color="white" />
+                  <Text className="text-white mt-2 text-3xl" style={{ fontFamily:Fonts.Inter }}>Schedule</Text>
+                </BentoBox>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => {}}>
+                <BentoBox size="small" className="border-fuchsia-400 w-40 h-40 rounded-3xl mb-5" style={{ backgroundColor: "rgba(217, 70, 239, 0.3)", borderWidth: 1 }}>
+                  <FontAwesome6 name="flag" size={30} color="white" />
+                  <Text className="text-white mt-2 text-3xl" style={{ fontFamily:Fonts.Inter }}>Alliance</Text>
+                </BentoBox>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => {}}>
+                <BentoBox size="small" className="border-blue-400 w-40 h-40 rounded-3xl" style={{ backgroundColor: "rgba(59, 130, 246, 0.3)", borderWidth: 1 }}>
+                  <FontAwesome6 name="qrcode" size={30} color="white" />
+                  <Text className="text-white mt-2 text-3xl" style={{ fontFamily:Fonts.Inter }}>QR</Text>
+                </BentoBox>
+              </TouchableOpacity>
+            </BentoGrid>
             
             <Button onClick={deleteSession} title="Logout"/>
 
