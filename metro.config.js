befaeaadc032148aae78851936bc21596672217c
paragrghoshal <ghoshsar000@students.centergrove.k12.in.us>
2025-08-05 17:37:44 -0400

    added desktop capabilities

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,17 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+config.watchFolders = config.watchFolders.filter(
+  folder => !folder.includes('src-tauri')
+);
+
+config.resolver.blockList = [
+  /src-tauri\/.*/,
+];
+
+module.exports = config;
  
 module.exports = withNativeWind(config, { input: './global.css' })
